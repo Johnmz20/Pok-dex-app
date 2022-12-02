d0d88214a117ad65ec02ca685f97b8791388fad2
john meraz <102620932+Johnmz20@users.noreply.github.com>
2022-12-02 01:07:14 -0500

    updated script.js
    changed the array, added forEach loop instead of for loop, and added IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,33 @@
-let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison'] },
-    { name: 'Ivysaur', height: 1, type: ['Grass', 'Poison'] },
-    { name: 'Venusaur', height: 2, type: ['Grass', 'Poison'] },
-    { name: 'Charmander', height: 0.6, type: 'Fire'},
-    { name: 'Charmeleon', height: 1.1, type: 'Fire'},
-    { name: 'Charizard', height: 1.7, type: ['Fire', 'Flying'] },
-    { name: 'Squirtle', height: 0.5, type: 'Water'},
-    { name: 'Wartortle', height: 1, type: 'Water'},
-    { name: 'Blastoise', height: 1.6, type: 'Water'}
-];
-//for loop says if the pokemon is bigger than 1 meter it prints out "wow that is big!"
+let pokemonRespository = (function () {
+    let pokemonList = [
+        { name: "Bulbasaur", height: 0.7, type: ["Grass", "Poison"] },
+        { name: "Ivysaur", height: 1, type: ["Grass", "Poison"] },
+        { name: "Venusaur", height: 2, type: ["Grass", "Poison"] },
+        { name: "Charmander", height: 0.6, type: "Fire" },
+        { name: "Charmeleon", height: 1.1, type: "Fire" },
+        { name: "Charizard", height: 1.7, type: ["Fire", "Flying"] },
+        { name: "Squirtle", height: 0.5, type: "Water" },
+        { name: "Wartortle", height: 1, type: "Water" },
+        { name: "Blastoise", height: 1.6, type: "Water" },
+    ];
 
-for (let i= 0; i< pokemonList.length; i++) {
-    if(pokemonList[i].height >= 1) {
-        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' wow, that is big!<br>')
+    return {
+        add: function(pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function() {
+            return pokemonList;
+        }
+    };
+})();
+console.log(pokemonRespository.getAll());
+
+pokemonRespository.add({ name: "Caterpie", height: 1, type: "grass",});
+
+pokemonRespository.getAll().forEach(function (pokemon) {
+    if (pokemon.height >= 1) {
+        document.write(pokemon.name + " (height: " + pokemon.height + ")" + " wow, that is big!<br>");
+    } else {
+        document.write(pokemon.name + " (height: " + pokemon.height + ")<br>");
     }
-    else{ document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')<br>')}
-}
+});
